cad974f26960603b77bf0dbfb3446f5c2bec5a27
2391881857 <2391881857@qq.com>
2024-12-02 17:21:41 +0900

    デプロイ設定の追加

--- cx330o-infra/workflow-engine/packages/frontend/@n8n/design-system/src/utils/form-event-bus.ts
+++ cx330o-infra/workflow-engine/packages/frontend/@n8n/design-system/src/utils/form-event-bus.ts
@@ -10,3 +10,4 @@ export type FormEventBus = ReturnType<typeof createFormEventBus>;
  * Creates a new event bus to be used with the `FormInputs` component.
  */
 export const createFormEventBus = createEventBus<FormEventBusEvents>;
+
