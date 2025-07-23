2769d89709fb1dbd7ee1f6ee1ab7585c5df4be10
pisces0230 <yibo.feb21@gmail.com>
2025-07-23 21:33:33 +0900

    ロケール追加

--- cx330o-crm/platform/packages/twenty-server/src/engine/core-modules/tool/tools/email-tool/types/email-tool-input.type.ts
+++ cx330o-crm/platform/packages/twenty-server/src/engine/core-modules/tool/tools/email-tool/types/email-tool-input.type.ts
@@ -3,3 +3,4 @@ import { type z } from 'zod';
 import { type EmailToolInputZodSchema } from 'src/engine/core-modules/tool/tools/email-tool/email-tool.schema';
 
 export type EmailToolInput = z.infer<typeof EmailToolInputZodSchema>;
+
