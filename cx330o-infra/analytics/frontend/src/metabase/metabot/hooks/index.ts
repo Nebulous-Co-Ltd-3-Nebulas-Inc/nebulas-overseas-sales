7e6ba9c7ffba2e11b2962bbdfecff87857630035
pisces0230 <yibo.feb21@gmail.com>
2026-03-10 20:50:04 +0900

    コンポーネント分割

--- cx330o-infra/analytics/frontend/src/metabase/metabot/hooks/index.ts
+++ cx330o-infra/analytics/frontend/src/metabase/metabot/hooks/index.ts
@@ -2,3 +2,4 @@ export { useMetabotEnabledEmbeddingAware } from "./use-metabot-embedding-aware-e
 export { useMetabotAgent } from "./use-metabot-agent";
 export { useMetabotAgentsManager } from "./use-metabot-agents-manager";
 export { useDashCardAnalysis } from "./useDashCardAnalysis";
+
